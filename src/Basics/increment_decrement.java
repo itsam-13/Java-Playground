dd99f807ae9c8ed48e4bcd0054c6390491c444a3
Abhineet <abhineet.am13@gmail.com>
2026-01-31 15:15:12 +0530

    Add increment and decrement example in Java

---
+++ src/Basics/increment_decrement.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class increment_decrement {
+    static void main() {
+        int x =5;
+        System.out.println(x++); // first use then increase
+        System.out.println(x);
+
+        int y=7;
+        System.out.println(++y); //first increase then use
+        System.out.println(y);
+
+        int z=5;
+        System.out.println(--z);
+        System.out.println(z);
+
+        int a=5;
+        System.out.println(a--);
+        System.out.println(a);
+    }
+}
